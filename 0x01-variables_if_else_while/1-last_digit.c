300efdce3e0f74a47bcdc5feb8f3717afb5062e3
kmark-n <seasonablesalt@gmail.com>
2022-12-09 22:29:36 -0800

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (lastn > 5)
 {
-prinf("Last digit of %d is %d and is greater than 5\n", n, lastn);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastn);
 }
 else if (lastn == 0)
 {
