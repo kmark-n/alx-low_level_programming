366c1691ea3e16fe907c1afad5e8c78b4b89d5f2
kmark-n <seasonablesalt@gmail.com>
2022-12-10 02:09:06 -0800

    Print combo!

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print a combo of single digit numbers
+ * should be in ascending order
+ * to be seperated by comma and space
+ * putchar function only
+ * to be used onlty 4 times
+ * Return: 0
+ */
+
+int main(void)
+{
+int a = 0;
+while (a < 10)
+{
+putchar(48 + a);
+if (a != 9)
+{
+putchar(',');
+putchar(' ');
+}
+a++;
+}
+putchar('\n');
+return (0);
+}
