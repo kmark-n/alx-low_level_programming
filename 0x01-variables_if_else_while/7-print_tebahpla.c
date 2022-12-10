f276cf18f73866e09b838eb118bf4246c1abe0f3
kmark-n <seasonablesalt@gmail.com>
2022-12-10 01:24:39 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * prints lowercase alphabet in reverse
+ * followed by a new line
+ * putchar function only
+ * to be used only twice
+ * Return: 0
+ */
+
+int main(void)
+{
+char 1 = 'z';
+while (1 >= 'a')
+{
+putchar(1);
+1--;
+}
+putchar('\n');
+return (0);
+}
