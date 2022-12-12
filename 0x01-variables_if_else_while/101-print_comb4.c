020791c972ed5875deb992c152dcfd23145ce20a
kmark-n <seasonablesalt@gmail.com>
2022-12-12 06:15:05 -0800

    Do what you do better...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,6 +14,7 @@ int main(void)
 int c;
 int d;
 int e = 0;
+
 while (e < 10)
 {
 d = 0;
@@ -27,12 +28,14 @@ if (c != d && d != e && e <d && d < c)
 putchar('0' + e);
 putchar('0' + d);
 putchar('0' + c);
+
 if (c + d + e != 9 + 8 + 7)
 {
 putchar(',');
 putchar(' ');
 }
 }
+
 c++;
 }
 d++;
