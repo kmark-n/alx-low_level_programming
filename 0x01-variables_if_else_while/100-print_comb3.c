162779ec1fda11c7938b95a422f4f03e89b82369
kmark-n <seasonablesalt@gmail.com>
2022-12-12 05:00:51 -0800

    The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,24 +12,28 @@
 
 int main(void)
 {
-int ones = '0';
-int tens = '0';
+int c;
+int d = 0;
 
-for (tens = '0'; tens < '9'; tens++)
+while (d < 10)
 {
-for (ones = '0'; ones < '9'; ones++)
+c = 0;
+while (c < 10)
 {
-if (!((ones == tens) || (tens > ones)))
+if (d != c && d < c)
 {
-putchar(ones);
-putchar(tens);
-if (!(ones == '9' && tens == '8'))
+putchar('0' + d);
+putchar('0' + c);
+
+if (c + d != 17)
 {
 putchar(',');
 putchar(' ');
 }
 }
+c++;
 }
+d++
 }
 putchar('\n');
 return (0);
