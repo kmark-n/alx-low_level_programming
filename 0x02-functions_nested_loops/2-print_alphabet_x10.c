eb9b5fb24ad5a861db195e7e92644c963bb27b36
kmark-n <seasonablesalt@gmail.com>
2022-12-14 01:26:36 -0800

    10x an alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+# include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+char c = 'a';
+
+while (c <= 10)
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
