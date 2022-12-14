0308af4b0ba00013c32f7e1db1e369f773d0bb45
kmark-n <seasonablesalt@gmail.com>
2022-12-14 00:14:04 -0800

    The alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+# include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * printing the alphabet in lowercase
+ * followed by a new line
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+char 1 = 'a';
+while (1 <= 'z')
+{
+_putchar(1);
+1++;
+}
+_putchar('\n');
+return (0);
+}
