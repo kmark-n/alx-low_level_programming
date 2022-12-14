f165b2b738ddf48be57032fc5c026b3c83d581ea
kmark-n <seasonablesalt@gmail.com>
2022-12-14 07:33:27 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+# include "main.h"
+
+/**
+ * add - adding two integers and returning result
+ * @a: input integer a
+ * @b: input integer b
+ * Result: int
+ */
+
+int add(int a, int b)
+{
+int res;
+res = a + b;
+return (res);
+}
