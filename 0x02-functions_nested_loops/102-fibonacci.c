c81fa1a1f14515905a3699e938889c331eb72617
kmark-n <seasonablesalt@gmail.com>
2022-12-14 21:52:13 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+# include <stdio.h>
+
+/**
+ * main - Entry point
+ * a program that prints first 50 fibonacci numbers
+ * starting with 1 and two
+ * Return: int
+ */
+
+int main(void)
+{
+long i = 0;
+long j = 0;
+int n = 0;
+long fib;
+
+while (n < 50)
+{
+fib = j + i;
+if (n != 49)
+{
+printf("%ld, ", fib);
+}
+else
+{
+printf("%ld, ", fib);
+}
+i = j;
+j = fib;
+n++;
+}
+return (0);
+}
