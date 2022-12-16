ee925571ec7c15f958d4ea853f6f122b4331d454
kmark-n <seasonablesalt@gmail.com>
2022-12-15 22:28:52 -0800

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - fuction that checks whether c is a digit
+ * @c: The input being checked
+ * Return: (0)
+ */
+
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
