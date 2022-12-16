7e554e4e957a17ed72f83dbfd2aa42146ffcd62e
kmark-n <seasonablesalt@gmail.com>
2022-12-15 23:16:40 -0800

    Numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints numbers
+ * from 0 t0 9
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+int i = 0;
+while (i < 10)
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
+}
