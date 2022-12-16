095954329e3619b8986adff0405f98fdba718ab2
kmark-n <seasonablesalt@gmail.com>
2022-12-15 22:49:00 -0800

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - function that checks for a digit
+ * @c: input value to check
+ * Return: int
+ */
+
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
