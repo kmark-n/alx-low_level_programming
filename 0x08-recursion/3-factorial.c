505d938c9314a8c3e6a6e9465506cf3bf5113858
kmark-n <seasonablesalt@gmail.com>
2023-01-07 01:19:53 -0800

    Function that returns the factorial of a given number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * factorial - function that returns the factorial 
+ * of a given number
+ * @n: assigned for an integer
+ * Return: int
+ */
+
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+if (n == 0)
+{
+return (1);
+}
+return (n * factorial(n - 1));
+}
