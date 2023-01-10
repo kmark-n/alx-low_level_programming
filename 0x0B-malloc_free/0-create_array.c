e9ec7c6c184a520a6616e146572cfbf91f042d0e
kmark-n <seasonablesalt@gmail.com>
2023-01-10 10:59:19 -0800

    Header file containing prototypes of some functions

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+/**
+ * *create_array - function that creates an array of chars, with a specific char
+ * @size: input size
+ * @c: input char
+ * Return: char
+ */
+
+char *create_array(unsigned int size, char c)
+{
+char *ar;
+unsigned int i = 0;
+
+if (size != 0)
+{
+ar = malloc(size * size of (char));
+}
+else
+{
+return (NULL);
+}
+
+if (ar == NULL)
+{
+return (NULL);
+}
+while (i < size)
+{
+ar[i] = c;
+i++;
+}
+return (ar);
+}
