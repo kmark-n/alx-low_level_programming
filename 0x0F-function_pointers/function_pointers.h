70239e92da5f237e86cbce456b1ee8047eccdb1e
kmark-n <seasonablesalt@gmail.com>
2023-01-21 02:37:34 -0800

    Prototypes of functions on C - function pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-
-#endif /*ifndef MAIN_H
