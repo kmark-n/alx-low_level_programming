efe3fc102f810d13a4f9c31d98f537915bc949dd
kmark-n <seasonablesalt@gmail.com>
2023-01-21 03:32:48 -0800

    function that executes a function given as a parameter on each element of an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,6 +4,7 @@
 /**
  * array_iterator - function that executes a function given as a parameter
  * on each element of an array
+ * @array: input
  * @size: size of the array
  * @action: pointer to the array
  * Return: (void)
