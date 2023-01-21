a540b4b589c7c18feafdb78f57c17e14cc80e105
kmark-n <seasonablesalt@gmail.com>
2023-01-21 02:24:21 -0800

    Prototypes of fuctions on C - function pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,8 @@
+#ifndef FILE_F_POINTERS
+#define FILE_F_POINTERS
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+
+#endif //ifndef FILE_F_POINTERS
