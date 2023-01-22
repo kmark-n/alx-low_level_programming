86f76f7e0d2a75431aa22808fe4eb8197efa1c3d
kmark-n <seasonablesalt@gmail.com>
2023-01-22 01:53:58 -0800

    Contains the function that selects the correct function to perform the operation asked by the user

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -11,17 +11,17 @@
 int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
-{"+", op_add};
-{"-", op_sub};
-{"*", op_div};
-{"%", op_mod};
-{NULL, NULL};
+{"+", op_add},
+{"-", op_sub},
+{"*", op_div},
+{"%", op_mod},
+{NULL, NULL}
 };
 
 int i = 0;
 while (i < 5)
 {
-if (*s == (ops[i]).op)
+if (*s == *(ops[i]).op)
 {
 return (*(ops[i]).f);
 }
