2863221cf32ea3b44f01a2352b34b16d344c3a07
kmark-n <seasonablesalt@gmail.com>
2023-01-22 00:36:49 -0800

    Contains operation functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - addition of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+return (a+b);
+}
+
+/**
+ * op_sub - subtraction of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: difference between a and b
+ */
+
+int op_sub(int a, int b)
+{
+return (a-b);
+}
+
+/**
+ * op_mul - multiplication of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: product of a and b
+ */
+
+int op_mul(int a, int b)
+{
+return (a*b);
+}
+
+/**
+ * op_div - division of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: division of a and b
+ */
+
+int op_div(int a, int b)
+{
+return (a/b);
+}
+
+/**
+ * op_mod - modulo of integer a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: remainder of diision of a by b
+ */
+
+int op_mod(int a, int b)
+{
+return (a%b);
+}
