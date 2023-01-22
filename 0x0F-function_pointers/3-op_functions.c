78f903875efbecb6432c8560cd1b14f3a53fda7a
kmark-n <seasonablesalt@gmail.com>
2023-01-22 01:56:02 -0800

    Contains functions

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -7,7 +7,7 @@
 
 int op_add(int a, int b)
 {
-return (a+b);
+return (a + b);
 }
 
 /**
@@ -19,7 +19,7 @@ return (a+b);
 
 int op_sub(int a, int b)
 {
-return (a-b);
+return (a - b);
 }
 
 /**
@@ -31,7 +31,7 @@ return (a-b);
 
 int op_mul(int a, int b)
 {
-return (a*b);
+return (a * b);
 }
 
 /**
@@ -43,7 +43,7 @@ return (a*b);
 
 int op_div(int a, int b)
 {
-return (a/b);
+return (a / b);
 }
 
 /**
@@ -55,5 +55,5 @@ return (a/b);
 
 int op_mod(int a, int b)
 {
-return (a%b);
+return (a % b);
 }
